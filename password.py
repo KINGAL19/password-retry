66e7b709117c1cc41e91e37790a007e40ebcc962
KINGAL <kingal171314@gmail.com>
2020-08-28 21:52:09 +0800

    basic version done

---
+++ password.py
@@ -0,0 +1,26 @@
+# x = 2
+# while x >= 0:
+# 	password = input('請輸入密碼: ')
+# 	if password == 'a123456':
+# 		print('登入成功')
+# 		break
+# 	if password != 'a123456':
+# 		print('密碼錯誤!還有', x, '次機會!')
+# 	x -= 1
+# if x < 0:
+# 	print('已錯誤3次', '請五分鐘後再試試!') 
+# --------------自己寫的
+
+password = 'a123456'
+i = 3
+while True:
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
+		print('登入成功')
+		break
+	else:
+		i -= 1
+		print('密碼錯誤! 還有', i, '次機會!')
+		if i == 0:
+			break
+	
