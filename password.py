cae137c43aea12c5f68d8f841cc3dd9f24b3691a
KINGAL <kingal171314@gmail.com>
2020-08-28 22:23:08 +0800

    change to while i > 0

--- password.py
+++ password.py
@@ -9,18 +9,45 @@
 # 	x -= 1
 # if x < 0:
 # 	print('已錯誤3次', '請五分鐘後再試試!') 
-# --------------自己寫的
+# ------------------------------------------自己寫的
 
+# password = 'a123456'
+# i = 3
+# while True:
+# 	pwd = input('請輸入密碼: ')
+# 	if pwd == password:
+# 		print('登入成功')
+# 		break
+# 	else:
+# 		i -= 1
+# 		print('密碼錯誤! 還有', i, '次機會!')
+# 		if i == 0:
+# 			break
+# ------------------------------------------老師寫的	
+# password = 'a123456'
+# i = 3
+# while i > 0:
+# 	pwd = input('請輸入密碼: ')
+# 	i -= 1
+# 	if pwd == password:
+# 		print('登入成功')
+# 		break
+# 	elif i > 0:
+# 		print('密碼錯誤! 還有', i, '次機會!')
+# 	else:
+# 		print('已錯誤3次', '請五分鐘後再試試!')
+# ------------------------------------------自己寫的
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
 	pwd = input('請輸入密碼: ')
+	i -= 1
 	if pwd == password:
 		print('登入成功')
 		break
 	else:
-		i -= 1
-		print('密碼錯誤! 還有', i, '次機會!')
-		if i == 0:
-			break
-	
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有', i, '次機會!')
+		else:
+			print('已錯誤3次', '請五分鐘後再試試!')
